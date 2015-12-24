51d2e2ff05e50585adc714e6d3db622ced654f2f
Greg Molnar <molnargerg@gmail.com>
2015-12-24 13:01:25 +0000

    render a different layout for iOS app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action -> { request.variant = :ios_app if request.user_agent == 'iOS App 1.0' }
 end
