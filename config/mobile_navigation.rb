8bb1fd536b7453cefec0b6f47ad4b3ee444c07b6
Greg Molnar <molnargerg@gmail.com>
2015-12-27 14:18:12 +0000

    mobile navigation tweaks

--- config/mobile_navigation.rb
+++ config/mobile_navigation.rb
@@ -4,11 +4,11 @@ SimpleNavigation::Configuration.run do |navigation|
   # Specify a custom renderer if needed.
   # The default renderer is SimpleNavigation::Renderer::List which renders HTML lists.
   # The renderer can also be specified as option in the render_navigation call.
-  navigation.renderer = SimpleNavigation::Renderer::Json
+  # navigation.renderer = SimpleNavigation::Renderer::Json
 
   # Define the primary navigation
   navigation.items do |primary|
-    primary.item :sign_in, 'Sign in', new_user_session_path, unless: -> { current_user }
-    primary.item :sign_in, 'Sign out', destroy_user_session_path, link: { data: { method: :delete } }, if: -> { current_user }
+    primary.item :sign_in, 'Sign in', new_user_session_url, unless: -> { current_user }
+    primary.item :sign_in, 'Sign out', destroy_user_session_url, link: { data: { method: :delete } }, if: -> { current_user }
   end
 end
